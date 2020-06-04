093dcce5fa94a8fea349d8592c26919dd1bbc35c
Marcos Velasco <devslife7@gmail.com>
2020-06-04 13:19:30 -0400

    initialize

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # your code goes here
+ 
+class Person
+
+    attr_reader :name
+
+
+    def initialize(name)
+      @name = name
+      @bank_balance = 25
+      @happiness_points = 8
+      @Hygiene_points = 8
+    end
+
+
+
+end
